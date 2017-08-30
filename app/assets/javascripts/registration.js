88e2c710639dfdd578feea11c23581973b597c87
Paulo Jansen <pauloo.jansen@gmail.com>
2017-08-30 17:33:52 -0300

    registration

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -3,6 +3,7 @@ document.addEventListener("turbolinks:load", function() {
         var step_1 = $('#registration-step-1');
         var step_2 = $('#registration-step-2');
         var step_3 = $('#registration-step-3');
+        var current_step = step_1;
 
         step_1.hide();
         step_2.hide();
@@ -11,23 +12,48 @@ document.addEventListener("turbolinks:load", function() {
         function toggle_step(step_from, step_to) {
             step_from.hide().hide();
             step_to.show().animateCss('fadeIn');
+            current_step = step_to;
         }
 
         step_1.show().animateCss('fadeIn');
 
         setTimeout(function () {
             toggle_step(step_1, step_2);
-        }, 2500);
+        }, 25);
 
         step_2.hide();
 
 
         $('#btn-continue-registration-step-2').click(function() {
-            toggle_step(step_2, step_3);
+            if(is_step_2_valid()) {
+                toggle_step(step_2, step_3);
+            }
         });
 
         $('#btn-back-registration-step-3').click(function() {
             toggle_step(step_3, step_2);
         });
+
+        $('#registration-form').on('keyup keypress', function(e) {
+            if(current_step == step_2) {
+                var keyCode = e.keyCode || e.which;
+                if (keyCode === 13) {
+                    if(is_step_2_valid()) {
+                        toggle_step(step_2, step_3);
+                    }
+                    e.preventDefault();
+                    return false;
+                }
+            }
+        });
+
+        function is_step_2_valid() {
+            var first_name = document.getElementById('registration_first_name').validity.valid;
+            var last_name = document.getElementById('registration_last_name').validity.valid;
+            if(first_name && last_name) {
+                return true;
+            }
+            return false;
+        }
     }
 });
