9de0914060df18693a81dad2b3f0486a1481ddce
Paulo Jansen <pauloo.jansen@gmail.com>
2017-09-12 14:17:30 -0300

    signin redirect, signin button, signout btn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource_or_scope)
+    if current_user.profile_is_complete?
+      projects_path
+    else
+      flash[:success] = 'Complete seu perfil!'
+      my_profile_path
+    end
+  end
+
 end
