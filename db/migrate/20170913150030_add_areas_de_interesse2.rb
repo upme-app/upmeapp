c12763d683d927f8d0a78beb7d8c031abd1a5de2
PAULO JANSEN <pauloo.jansen@gmail.com>
2017-09-13 12:07:12 -0300

    areas de itneresse 2

---
+++ db/migrate/20170913150030_add_areas_de_interesse2.rb
@@ -0,0 +1,22 @@
+class AddAreasDeInteresse2 < ActiveRecord::Migration[5.0]
+  def change
+    AreaDeInteresse.create({name: 'Contabilidade'})
+    AreaDeInteresse.create({name: 'Economia'})
+    AreaDeInteresse.create({name: 'Letras'})
+    AreaDeInteresse.create({name: 'Cinema e animação'})
+    AreaDeInteresse.create({name: 'Moda'})
+    AreaDeInteresse.create({name: 'Veterinária'})
+    AreaDeInteresse.create({name: 'Biológicas'})
+    AreaDeInteresse.create({name: 'Saúde'})
+    AreaDeInteresse.create({name: 'Matemática'})
+    AreaDeInteresse.create({name: 'Agronomia'})
+    AreaDeInteresse.create({name: 'Finanças'})
+    AreaDeInteresse.create({name: 'Letras'})
+    AreaDeInteresse.create({name: 'Cinema e animação'})
+    AreaDeInteresse.create({name: 'Moda'})
+    AreaDeInteresse.create({name: 'Veterinária'})
+    AreaDeInteresse.create({name: 'Biológicas'})
+    AreaDeInteresse.create({name: 'Saúde'})
+    AreaDeInteresse.create({name: 'Publicidade e propaganda'})
+  end
+end
