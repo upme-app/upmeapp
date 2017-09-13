81c11e6e3ac3dfe3294dfc0cecc04b4a89343f42
PAULO JANSEN <pauloo.jansen@gmail.com>
2017-09-13 17:45:09 -0300

    project user route validation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :authorize_project, except: [:show_public, :index]
 
   def new
     @project = Project.new
@@ -107,4 +108,13 @@ class ProjectsController < ApplicationController
   def set_project
     @project = Project.find(params[:id])
   end
+
+  def authorize_project
+    set_project
+    unless @project.has_user(current_user)
+      flash[:danger] = 'Você não pode acessar essa página'
+      redirect_to public_project_path(@project.id)
+    end
+  end
+
 end
