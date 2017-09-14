45c83f2648c2894098ef66d5a5aacb937d8b9c8b
PAULO JANSEN <pauloo.jansen@gmail.com>
2017-09-14 11:10:55 -0300

    init rspec rails

---
+++ spec/models/user.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+
+  # projetos que o usuário é membro
+  it "user projects" do
+   
+  end
+
+  # convites que ele pode aceitar para virar um membro
+  it "user invitations" do
+  
+  end
+
+  # se o usuario pode comentar em uma timeline
+  it "user can timeline comment" do
+  
+  end
+
+end
