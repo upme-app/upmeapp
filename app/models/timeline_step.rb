b20b9a74d6dbedd67888e2e53a1d2ea89ec79b01
PAULO JANSEN <pauloo.jansen@gmail.com>
2017-09-27 14:05:26 -0300

    add other steps

--- app/models/timeline_step.rb
+++ app/models/timeline_step.rb
@@ -3,15 +3,44 @@ class TimelineStep < ApplicationRecord
   has_many :timeline_comments
 
   def self.init_project_steps(project)
-
     create_step(
         project,
         'Confirmação de Match',
-        'Conheça sua equipe ou seu cliente, alinhe as expectativas, explique o escopo do projeto e confirme o match!',
-        'Primeira reunião entre cliente e time realizada com sucesso.',
+        'Diagnóstico da situação da empresa, alinhamento de expectativas entre as partes, definição do escopo do projeto - o que vai ser entregue e como; briefing detalhado; definição sobre elaboração de contrato (se necessário), etc.',
+        'Primeira reunião entre cliente e equipe realizada com sucesso - presencialmente ou via web.',
         Time.now + 2.weeks
     )
+  end
 
+  def self.create_other_steps(project)
+    create_step(
+        project,
+        'Desenvolvimento 1',
+        'Brainstorm da equipe, definição das melhorias, principais pontos a serem trabalhados e start no desenvolvimento do projeto.',
+        'Elaborar o diagnóstico com os detalhes a serem trabalhados. ',
+        Time.now + 4.weeks
+    )
+    create_step(
+        project,
+        'Reunião intermediária',
+        'Feedback do cliente sobre o diagnóstico, propor alterações e responder às demandas dos alunos. Pré-aprovação do esboço do projeto; referências; hora de tirar dúvidas e comunicar se existe algum documento ou informação faltando para a elaboração do projeto.',
+        'Reunião realizada com sucesso.',
+        Time.now + 5.weeks
+    )
+    create_step(
+        project,
+        'Desenvolvimento 2',
+        'Seguir o desenvolvimento do projeto com as alterações propostas pelo cliente, com a documentação completa e as informações em dia.',
+        'Conclusão do projeto.',
+        Time.now + 9.weeks
+    )
+    create_step(
+        project,
+        'Entrega',
+        'Apresentação final ao professor, realizar ajustes sugeridos e entregar o trabalho para cliente.',
+        'Entrega do projeto pelo time e feedback do cliente',
+        Time.now + 10.weeks
+    )
   end
 
   def self.create_step(project, title, desc, entregavel, data_entrega)
@@ -23,6 +52,5 @@ class TimelineStep < ApplicationRecord
       entrega: data_entrega
     })
   end
-
 
 end
