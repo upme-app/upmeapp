b3298327a095a6c19b1073b0279c66ffb15b0973
PAULO JANSEN <pauloo.jansen@gmail.com>
2017-10-05 14:26:51 -0300

    fixing timezone

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Upmeapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Brasilia'
   end
 end
 
