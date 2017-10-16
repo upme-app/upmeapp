63e314d70118e5b296b8274885fc34d3c36c73d2
PAULO JANSEN <pauloo.jansen@gmail.com>
2017-10-16 16:11:42 -0200

    sidekiq

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,14 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+
+  config.active_job.queue_adapter = :sidekiq
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.smtp_settings = {
