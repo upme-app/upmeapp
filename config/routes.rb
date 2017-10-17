b687d22a5c4664b356c40c79bac3a6ede6e2263a
PAULO JANSEN <pauloo.jansen@gmail.com>
2017-10-17 16:26:04 -0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,8 @@ Rails.application.routes.draw do
   match 'projetos/:id/enviar-solicitacao-membro', to: 'projects#add_member_solicitation', via: :post, as: :add_member_solicitation
 
   match 'meu-perfil', to: 'profile#my_profile', via: :get, as: :my_profile
+  match 'meu-perfil/editar-dados-de-cobranca', to: 'profile#edit_billing', via: :get, as: :edit_billing
+  match 'meu-perfil/editar-dados-de-cobranca', to: 'profile#edit_billing', via: :get, as: :save_billing
   match 'meu-perfil/editar', to: 'profile#edit_profile', via: :get, as: :edit_profile
   match 'meu-perfil/editar', to: 'profile#save_profile', via: :post, as: :save_profile
   match 'meu-perfil/salvar_foto', to: 'profile#update_picture', via: :post, as: :update_picture
