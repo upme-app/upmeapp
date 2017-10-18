4078034abb98cf1897a95f49de364d53605af042
PAULO JANSEN <pauloo.jansen@gmail.com>
2017-10-18 20:42:38 -0200

    envia pro explorar dps de fazer conta

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -74,7 +74,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    my_profile_path
+    explore_path
   end
 
   # The path used after sign up for inactive accounts.
