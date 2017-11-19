34e41331b333916369465b59634b75c809b392d1
Marcelo Perini Veloso <marcelo.perini.veloso@gmail.com>
2017-11-19 12:21:29 -0200

    finish webhook payment notification

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,5 +1,7 @@
 class ChargesController < ApplicationController
   before_action :set_project, only: [:create]
+  before_action :webhook_verification, only: :payment_notification
+  before_action :set_payment, only: :payment_notification
 
   rescue_from Stripe::CardError, with: :catch_exception
 
@@ -14,26 +16,12 @@ class ChargesController < ApplicationController
 
   def payment_notification
     binding.pry
-    payload = request.body.read
-    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
-    event = nil
-
     respond_to do |format|
       format.json do
-        begin
-          binding.pry
-          event = Stripe::Webhook.construct_event(payload, sig_header,
-                                                  Rails.application.secrets.endpoint_secret)
-          binding.pry
-        rescue JSON::ParserError => e
-          render status: 400
-          return
-        rescue Stripe::SignatureVerificationError => e
-          render status: 400
-          return
+        if params[:data][:object][:paid]
+          @payment.update_attribute(:status, 0)
+          render json: { success: true }, status: 200
         end
-
-        render json: { success: true }, status: 200
       end
     end
   end
@@ -48,7 +36,35 @@ class ChargesController < ApplicationController
     @project = Project.find(charges_params[:project_id].to_i)
   end
 
+  def set_payment
+    @payment = Payment.find(params[:data][:object][:metadata][:payment_id])
+  end
+
   def catch_exception(exception)
     flash[:error] = exception.message
+  end
+
+  def webhook_verification
+    return if verify_webhook
+    respond_to do |format|
+      format.json { return render json: { success: false }, status: 400 }
+    end
+  end
+
+  def verify_webhook
+    payload = request.body.read
+    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+    event = nil
+
+    begin
+      event = Stripe::Webhook.construct_event(payload, sig_header,
+                                              Rails.application.secrets.stripe_endpoint_secret)
+    rescue JSON::ParserError => e
+      return false
+    rescue Stripe::SignatureVerificationError => e
+      return false
+    end
+
+    true
   end
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,6 +2,4 @@ class Payment < ApplicationRecord
   belongs_to :user
   belongs_to :project
   enum status: [:pago, :aguardando, :recusado]
-
-
 end
