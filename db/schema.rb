cb72a51ad174f602ef95dff7eb9c1b854da81b38
PAULO JANSEN <pauloo.jansen@gmail.com>
2017-11-23 16:58:53 -0200

    project uploads model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118214442) do
+ActiveRecord::Schema.define(version: 20171123185825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,6 +117,16 @@ ActiveRecord::Schema.define(version: 20171118214442) do
     t.index ["project_id"], name: "index_project_invitations_on_project_id", using: :btree
     t.index ["user_from_id"], name: "index_project_invitations_on_user_from_id", using: :btree
     t.index ["user_to_id"], name: "index_project_invitations_on_user_to_id", using: :btree
+  end
+
+  create_table "project_uploads", force: :cascade do |t|
+    t.integer  "project_id"
+    t.string   "file"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_project_uploads_on_project_id", using: :btree
+    t.index ["user_id"], name: "index_project_uploads_on_user_id", using: :btree
   end
 
   create_table "project_users", force: :cascade do |t|
@@ -440,6 +450,8 @@ ActiveRecord::Schema.define(version: 20171118214442) do
   add_foreign_key "project_area_de_interesses", "area_de_interesses", column: "area_de_interesse_id"
   add_foreign_key "project_area_de_interesses", "projects"
   add_foreign_key "project_invitations", "projects"
+  add_foreign_key "project_uploads", "projects"
+  add_foreign_key "project_uploads", "users"
   add_foreign_key "project_users", "projects"
   add_foreign_key "project_users", "users"
   add_foreign_key "projects", "users"
