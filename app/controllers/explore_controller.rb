0bfbe9c2ee1a287e36811f03f74b4cd89c99da94
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2017-11-27 14:11:41 -0200

    Alterando as categorias

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -3,12 +3,12 @@ class ExploreController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @all_areas = AreaDeInteresse.where(name: ["Engenharias", "Arquitetura", "Administração"])
+    @all_areas = AreaDeInteresse.where(name: ["Engenharias", "Marketing", "Administração"])
 
     if params and params[:areas_interesse].present?
       @selected_areas = AreaDeInteresse.where(id: params[:areas_interesse].map { |item| item.to_i})
     else
-      @selected_areas = AreaDeInteresse.where(name: ["Engenharias", "Arquitetura", "Administração"])
+      @selected_areas = AreaDeInteresse.where(name: ["Engenharias", "Marketing", "Administração"])
     end
 
     @avaliable_projects = current_user.available_projects(@selected_areas.map { |item| item.id})
