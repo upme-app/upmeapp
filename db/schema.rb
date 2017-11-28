63b1fb2911b1276e0c3315a6e0cd3a5adde8d10b
PAULO JANSEN <pauloo.jansen@gmail.com>
2017-11-28 02:50:37 -0200

    timeline and feedback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127221113) do
+ActiveRecord::Schema.define(version: 20171128042112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -397,6 +397,7 @@ ActiveRecord::Schema.define(version: 20171127221113) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.datetime "check_date"
+    t.text     "feedback"
     t.index ["project_id"], name: "index_timeline_steps_on_project_id", using: :btree
   end
 
